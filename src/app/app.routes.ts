43727cb22a7c9cac7080b215253bbf44a7ff2e33
Kacper Mejsner <kacpermejs@gmail.com>
2023-12-10 18:21:13 +0100

    game component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path: 'home', loadComponent: () => import('./pages/home/home.component').then(routes => routes.HomeComponent)},
+    {path: 'game', loadComponent: () => import('./pages/game/game.component').then(routes => routes.GameComponent)},
     {path: 'ranking', loadChildren: () => import('./pages/ranking/ranking.routes').then(routes => routes.RankingRoutes) },
 
     // Redirection:
