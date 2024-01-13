94c46fed557190dd3048dca553b3fd8cf3318ecc
Kacper Mejsner <kacpermejs@gmail.com>
2024-01-13 13:59:31 +0100

    Docker container as domain in production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://127.0.0.1:8080', //process.env['API_HOST'],
-    apiWebsocket: 'http://127.0.0.1:8080/websocket', //process.env['HOST_WEBSOCKET'] + '/websocket',
+    apiUrl: 'http://backend-prod:8080', //process.env['API_HOST'],
+    apiWebsocket: 'http://backend-prod:8080/websocket', //process.env['HOST_WEBSOCKET'] + '/websocket',
 
     cognito: {
         userPoolId: 'us-east-1_R73Wa0n55',
