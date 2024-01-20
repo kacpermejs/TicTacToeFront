f1e995993367a0a95af538ec8039464206ff7928
Kacper Mejsner <kacpermejs@gmail.com>
2024-01-20 23:33:18 +0100

    domain websocket

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: 'http://tictactoe-env.eba-mn2mtuqg.us-east-1.elasticbeanstalk.com:8080', //process.env['API_HOST'],
-    apiWebsocket: 'http://35.171.139.155:8080/websocket', //process.env['HOST_WEBSOCKET'] + '/websocket',
+    apiWebsocket: 'http://tictactoe-env.eba-mn2mtuqg.us-east-1.elasticbeanstalk.com:8080/websocket', //process.env['HOST_WEBSOCKET'] + '/websocket',
 
     cognito: {
         userPoolId: 'us-east-1_R73Wa0n55',
